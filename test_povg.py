95027e797c132622492aa8dd30bc67a7c93788aa
Tim Pederick <pederick@gmail.com>
2013-09-13 23:38:04 +0800

    Got the one existing test running again.

--- test_povg.py
+++ test_povg.py
@@ -9,21 +9,17 @@ egl.eglInitialize(egl.eglGetCurrentDisplay(),
                   ctypes.byref(ctypes.c_int(1)),
                   ctypes.byref(ctypes.c_int(4)))
 
-# 1. Can we import PVG?
+# 1. Can we import Povg?
 import povg
-from povg.context import Context, name_lookup, FillRule
+from povg.context import Context, FillRule
 
 # 2. Can we establish a context?
 ctx = Context()
 
 # 3. Can we get some context values?
 print('Maximum kernel size is {}.'.format(ctx.max_kernel_size))
-print('Initial fill rule is {}, AKA {}.'.format(ctx.fill_rule,
-                                                name_lookup(FillRule,
-                                                            ctx.fill_rule)))
+print('Initial fill rule is {}.'.format(ctx.fill_rule))
 
 # 4. Can we set some context values?
-ctx.fill_rule = FillRule.even_odd
-print('Fill rule is now {}, AKA {}.'.format(ctx.fill_rule,
-                                            name_lookup(FillRule,
-                                                        ctx.fill_rule)))
+ctx.fill_rule = FillRule.NON_ZERO
+print('Fill rule is now {}.'.format(ctx.fill_rule))
